79230764427ab17955182f13719e425ed4f8e30f
RamgiriNaveen <naveen.ramgiri@gspann.com>
2022-09-15 20:34:06 +0530

    tailwind css

--- management-system/src/pages/Home.js
+++ management-system/src/pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
 import "firebase/compat/firestore";
 import '../styles/Home.css';
-import Herosection from "./Herosection";
 function Home() {
   return (
-    <div className="myform container">
-      <div>
-        <h1>Fill me with product Table : ag-grid </h1>
-      </div>
-      <Herosection />
+    <div className="myform lg:container lg:mx-auto">
+      <img src="https://www.cyberstockroom.com/Features/Wallpapers/ProductImages/wallpaper-tiny.png" />
     </div>
   )
 }
