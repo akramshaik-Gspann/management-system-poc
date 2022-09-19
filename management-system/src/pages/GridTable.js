f0032f4ec73164fa5fbae89086023eda7d70f28b
Kumar Shoolam <110256940+kumarindian2008@users.noreply.github.com>
2022-09-19 18:23:51 +0530

    Added Filter by Catalog Type dropdown into AG GRID Table..!!

--- management-system/src/pages/GridTable.js
+++ management-system/src/pages/GridTable.js
@@ -1,120 +1,194 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import { Grid, Button } from '@material-ui/core'
-import FormDialog from '../Component/dialog';
-const initialValue = { catalogId: "", catalogType: "", itemName: "", priceNumber: "", color: "", Stock: "", lastUpdated: "",}
+import React, { useCallback, useState, useEffect, useRef } from "react";
+import "../App.css";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { Grid, Button } from "@material-ui/core";
+import FormDialog from "../Component/dialog";
+
+const initialValue = {
+  catalogId: "",
+  catalogType: "",
+  itemName: "",
+  priceNumber: "",
+  color: "",
+  Stock: "",
+  lastUpdated: "",
+};
+
 function Gridtable() {
-  const [gridApi, setGridApi] = useState(null)
-  const [tableData, setTableData] = useState(null)
+  const [gridApi, setGridApi] = useState(useRef());
+  const gridRef = useRef();
+  const [tableData, setTableData] = useState(null);
   const [open, setOpen] = React.useState(false);
-  const [formData, setFormData] = useState(initialValue)
+  const [formData, setFormData] = useState(initialValue);
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
-    setFormData(initialValue)
+    setFormData(initialValue);
   };
-  const url = `http://localhost:4000/users`
+  const url = `http://localhost:4000/users`;
   const columnDefs = [
     { headerName: "Item No", field: "id" },
     { headerName: "Catalog ID", field: "catalogId" },
     { headerName: "Catalog Type", field: "catalogType" },
     { headerName: "Item Name", field: "itemName" },
-    { headerName: "Price", field: "priceNumber", },
+    { headerName: "Price", field: "priceNumber" },
     { headerName: "Color", field: "color" },
     { headerName: "Stock", field: "Stock" },
     { headerName: "Last Updated", field: "lastUpdated" },
     {
-      headerName: "Actions", field: "id", cellRendererFramework: (params) => <div>
-        <Button variant="outlined" color="primary" onClick={() => handleUpdate(params.data)}>Edit</Button>
-        <Button variant="outlined" color="secondary" onClick={() => handleDelete(params.value)}>Delete</Button>
-      </div>
-    }
-  ]
-  // calling getUsers function for first time 
+      headerName: "Actions",
+      field: "id",
+      cellRendererFramework: (params) => (
+        <div>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => handleUpdate(params.data)}
+          >
+            Edit
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => handleDelete(params.value)}
+          >
+            Delete
+          </Button>
+        </div>
+      ),
+    },
+  ];
+  // calling getUsers function for first time
   useEffect(() => {
-    getUsers()
-  }, [])
+    getUsers();
+  }, []);
 
   //fetching user data from server
   const getUsers = () => {
-    fetch(url).then(resp => resp.json()).then(resp => setTableData(resp))
-  }
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((resp) => setTableData(resp));
+  };
   const onChange = (e) => {
-    const { value, id } = e.target
+    const { value, id } = e.target;
     // console.log(value,id)
-    setFormData({ ...formData, [id]: value })
-  }
+    setFormData({ ...formData, [id]: value });
+  };
   const onGridReady = (params) => {
-    setGridApi(params)
-  }
+    setGridApi(params);
+  };
 
   // setting update row data to form data and opening pop up window
   const handleUpdate = (oldData) => {
-    setFormData(oldData)
-    handleClickOpen()
-  }
+    setFormData(oldData);
+    handleClickOpen();
+  };
   //deleting a user
   const handleDelete = (id) => {
-    const confirm = window.confirm("Are you sure, you want to delete this row", id)
+    const confirm = window.confirm(
+      "Are you sure, you want to delete this row",
+      id
+    );
     if (confirm) {
-      fetch(url + `/${id}`, { method: "DELETE" }).then(resp => resp.json()).then(resp => getUsers())
-
+      fetch(url + `/${id}`, { method: "DELETE" })
+        .then((resp) => resp.json())
+        .then((resp) => getUsers());
     }
-  }
+  };
   const handleFormSubmit = () => {
     if (formData.id) {
-      //updating a user 
-      const confirm = window.confirm("Are you sure, you want to update this row ?")
-      confirm && fetch(url + `/${formData.id}`, {
-        method: "PUT", body: JSON.stringify(formData), headers: {
-          'content-type': "application/json"
-        }
-      }).then(resp => resp.json())
-        .then(resp => {
-          handleClose()
-          getUsers()
-
+      //updating a user
+      const confirm = window.confirm(
+        "Are you sure, you want to update this row ?"
+      );
+      confirm &&
+        fetch(url + `/${formData.id}`, {
+          method: "PUT",
+          body: JSON.stringify(formData),
+          headers: {
+            "content-type": "application/json",
+          },
         })
+          .then((resp) => resp.json())
+          .then((resp) => {
+            handleClose();
+            getUsers();
+          });
     } else {
       // adding new user
       fetch(url, {
-        method: "POST", body: JSON.stringify(formData), headers: {
-          'content-type': "application/json"
-        }
-      }).then(resp => resp.json())
-        .then(resp => {
-          handleClose()
-          getUsers()
-        })
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((resp) => {
+          handleClose();
+          getUsers();
+        });
     }
-  }
+  };
 
   const defaultColDef = {
     sortable: true,
-    flex: 1, filter: true,
+    flex: 1,
+    filter: true,
     // floatingFilter: true
-  }
+  };
+
+  //Filter by Catalog Type - Starts
+
+  const onFilterTextBoxChanged = useCallback(() => {
+    gridRef.current.api.setQuickFilter(
+      document.getElementById("filter-text-box").value
+    );
+    console.log(gridRef, "gridRef");
+  }, []);
+
+  //Filter by Catalog Type - Ends
+
   return (
     <div className="App">
       <Grid align="left">
-        <Button variant="contained" color="primary" onClick={handleClickOpen}>Add Product</Button>
+        <Button variant="contained" color="primary" onClick={handleClickOpen}>
+          Add Product
+        </Button>
       </Grid>
-      <div className="ag-theme-alpine" style={{ height: '400px' }}>
+      <Grid align="right">
+        {/* <Button variant="contained" color="primary" onClick={handleClickOpen}>
+          filter
+        </Button> */}
+        <select id="filter-text-box" onChange={onFilterTextBoxChanged}>
+          <option value="All">All</option>
+          <option value="Jeans">Jeans</option>
+          <option value="Shirts">Shirts</option>
+          <option value="Trousers">Trousers</option>
+          <option value="Jumpers">Jumpers</option>
+        </select>
+      </Grid>
+      <div className="ag-theme-alpine" style={{ height: "400px" }}>
         <AgGridReact
+          ref={gridRef}
           rowData={tableData}
           columnDefs={columnDefs}
           defaultColDef={defaultColDef}
           onGridReady={onGridReady}
         />
       </div>
-      <FormDialog open={open} handleClose={handleClose}
-        data={formData} onChange={onChange} handleFormSubmit={handleFormSubmit} />
+      <FormDialog
+        open={open}
+        handleClose={handleClose}
+        data={formData}
+        onChange={onChange}
+        handleFormSubmit={handleFormSubmit}
+      />
     </div>
   );
 }
