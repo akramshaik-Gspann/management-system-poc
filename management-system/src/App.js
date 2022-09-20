c406b26f09cc055bbdfa05032910c4fde6ba6539
Rabbu-G <105190642+Rabbu-G@users.noreply.github.com>
2022-09-20 12:40:24 +0530

    .

--- management-system/src/App.js
+++ management-system/src/App.js
@@ -1,217 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import { Grid, Button } from '@material-ui/core';
-import FormDialog from '../Component/dialog';
-import {Data} from '../Component/Uplod/Data';
-import * as XLSX from 'xlsx';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { useDispatch } from "react-redux";
+import { auth } from "./firebase";
+import { setUser } from './redux/actions';
+import Header from './pages/Header';
+import store from './redux/store';
+import Gridtable from './pages/GridTable';
+import Uplod from './Component/Uplod/Uplod';
+import Ag from './Component/Uplod/Ag';
 
 
-const initialValue = { catalogId: "", catalogType: "", itemName: "", priceNumber: "", color: "", Stock: "", lastUpdated: "",}
-function Gridtable() {
-  const [gridData, setGridData] = useState([])
-  const [gridApi, setGridApi] = useState(null)
-  const [tableData, setTableData] = useState(null)
-  const [open, setOpen] = React.useState(false);
-  const [formData, setFormData] = useState(initialValue)
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-    setFormData(initialValue)
-  };
-  const url = `http://localhost:4000/users`
-  const columnDefs = [
-    { headerName: "Item No", field: "id" },
-    { headerName: "Catalog ID", field: "catalogId" },
-    { headerName: "Catalog Type", field: "catalogType" },
-    { headerName: "Item Name", field: "itemName" },
-    { headerName: "Price", field: "priceNumber", },
-    { headerName: "Color", field: "color" },
-    { headerName: "Stock", field: "Stock" },
-    { headerName: "Last Updated", field: "lastUpdated" },
-    {
-      headerName: "Actions", field: "id", cellRendererFramework: (params) => <div>
-        <Button variant="outlined" color="primary" onClick={() => handleUpdate(params.data)}>Edit</Button>
-        <Button variant="outlined" color="secondary" onClick={() => handleDelete(params.value)}>Delete</Button>
-      </div>
-    }
-  ]
+function App() {
+  const dispatch = useDispatch();
+  console.log("Store", store.getState())
+  const [profile, setProfile] = useState();
   useEffect(() => {
-  // calling getUsers function for first time 
-  setGridData(gridData)
-}, [gridData])
-
-  useEffect(() => {
-    getUsers()
-  }, [])
-
-  //fetching user data from server
-  const getUsers = () => {
-    fetch(url).then(resp => resp.json()).then(resp => setTableData(resp))
-  }
-  const onChange = (e) => {
-    const { value, id } = e.target
-    // console.log(value,id)
-    setFormData({ ...formData, [id]: value })
-  }
-  const onGridReady = (params) => {
-    setGridApi(params)
-  }
-
-  // setting update row data to form data and opening pop up window
-  const handleUpdate = (oldData) => {
-    setFormData(oldData)
-    handleClickOpen()
-  }
-  //deleting a user
-  const handleDelete = (id) => {
-    const confirm = window.confirm("Are you sure, you want to delete this row", id)
-    if (confirm) {
-      fetch(url + `/${id}`, { method: "DELETE" }).then(resp => resp.json()).then(resp => getUsers())
-
-    }
-  }
-  const handleFormSubmit = () => {
-    if (formData.id) {
-      //updating a user 
-      const confirm = window.confirm("Are you sure, you want to update this row ?")
-      confirm && fetch(url + `/${formData.id}`, {
-        method: "PUT", body: JSON.stringify(formData), headers: {
-          'content-type': "application/json"
-        }
-      }).then(resp => resp.json())
-        .then(resp => {
-          handleClose()
-          getUsers()
-
-        })
-    } else {
-      // adding new user
-      fetch(url, {
-        method: "POST", body: JSON.stringify(formData), headers: {
-          'content-type': "application/json"
-        }
-      }).then(resp => resp.json())
-        .then(resp => {
-          handleClose()
-          getUsers()
-        })
-    }
-    }
-
-  const defaultColDef = {
-    sortable: true,
-    flex: 1, filter: true,
-    // floatingFilter: true
-  }
-
-  function ImportData() {
-  
-    const [excelFile, setExcelFile]=useState(null);
-    const [excelFileError, setExcelFileError]=useState(null);
-   
-    const [excelData, setExcelData]=useState(null);
-  
-    const fileType=['application/vnd.ms-excel'];
-    const handleFile = (e)=>{
-      let selectedFile = e.target.files[0];
-      if(selectedFile){
-  
-        if(selectedFile&&fileType.includes(selectedFile.type)){
-          let reader = new FileReader();
-          reader.readAsArrayBuffer(selectedFile);
-          reader.onload=(e)=>{
-            setExcelFileError(null);
-            setExcelFile(e.target.result);
-            const workbook = XLSX.read(e.target.result,{type:'buffer'});
-            const worksheetName = workbook.SheetNames[0];
-            const worksheet=workbook.Sheets[worksheetName];
-            const data = XLSX.utils.sheet_to_json(worksheet);
-            setExcelData(data);
-            alert("imported Succefully")
-          } 
-          
-        }
-        else{
-          setExcelFileError('Please select only excel file types');
-          setExcelFile(null);
-        }
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setProfile(authUser)
+        dispatch(setUser(authUser))
+      } else {
+        dispatch(setUser(null))
       }
-      else{
-        console.log('plz select your file');
-      }
-    }
-    
-    return (
-      <div className="container uplodedata">
-  
-        <div className='form'>
-          <form className='form-group' autoComplete="off">
-            {/* <label><h5>Upload Excel file</h5></label> */}
-            <br></br>
-            <input type='file' className='form-control'
-            onChange={handleFile} required></input>                  
-            {excelFileError&&<div className='text-danger'
-            style={{marginTop:5+'px'}}>{excelFileError}</div>}
-            
-          </form>
-        </div>
-  
-        <br></br>
-        {/* <hr></hr> */}
-  
-  
-        {/* <h5>View Excel file</h5> */}
-        <div className='viewer'>
-          {/* {excelData===null&&<>No file selected</>} */}
-          {excelData!==null&&(
-            <div className='table-responsive'>
-              <table className='table'>
-                <thead>
-                  <tr>
-                    <th scope='col'>Name</th>
-                    <th scope='col'>Account</th>
-                    <th scope='col'>Call</th>
-                    <th scope='col'>Minutes</th>
-                    <th scope='col'>Child</th>
-                    {/* <th scope='col'>Age</th>
-                    <th scope='col'>Date</th>                   */}
-                  </tr>
-                </thead>
-                <tbody>
-                  <Data excelData={excelData}/>
-                </tbody>
-              </table>            
-            </div>
-          )}       
-        </div>
-  
-      </div>
-    );
-  }
+    })
+  }, [dispatch])
+
   return (
-    <div className="App container">
-      <Grid align="left">
-        <ImportData />
-        <Button variant="contained" color="primary" onClick={handleClickOpen}>Add Product</Button>
-      </Grid>
-      <div className="ag-theme-alpine" style={{ height: '400px' }}>
-        <AgGridReact
-          rowData={tableData}
-          columnDefs={columnDefs}
-          defaultColDef={defaultColDef}
-          onGridReady={onGridReady}
-        />
-      </div>
-      <FormDialog open={open} handleClose={handleClose}
-        data={formData} onChange={onChange} handleFormSubmit={handleFormSubmit} />
-    </div>
+    <Router>
+       
+        <Header profile={profile}  setProfile={setProfile}/>
+        <div className="App">
+          <Routes>
+          <Route exact path='/' element={<Register/>} />
+            <Route exact path='/gridtable' element={<Gridtable/>} />
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/register' element={<Register />} />
+            <Route exact path='/uplod' element={<Uplod />} />
+            <Route exact path='/ag' element={<Ag />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
-
-export default Gridtable;
+export default App;
