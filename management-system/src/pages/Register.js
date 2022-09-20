071cf8ed7f019efa2fd43dfcc219c4e88e0baee0
RamgiriNaveen <naveen.ramgiri@gspann.com>
2022-09-20 11:49:05 +0530

    login changes

--- management-system/src/pages/Register.js
+++ management-system/src/pages/Register.js
@@ -43,8 +43,8 @@ const Register = () => {
         <div className='container'>
             <section className="main-form">
                 <div className="px-6 h-full text-gray-800">
-                    <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
-                        <div className='form-left grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0'>
+                    <div className="flex xl:justify-center h-screen lg:justify-between justify-center items-center flex-wrap h-full g-6">
+                        <div className='form-left hidden lg:block grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0'>
                             <img src={formimg} />
                         </div>
                         <div className="xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
